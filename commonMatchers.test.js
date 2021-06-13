055ce5a2d281b4e1ee6aac8f0892f46a13bb5277
berkcanozbalci966 <54132791+berkcanozbalci966@users.noreply.github.com>
2021-06-14 00:20:48 +0300

    common & truthiness matchers

---
+++ commonMatchers.test.js
@@ -0,0 +1,27 @@
+const sum = require("./sum");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("two plus two is four", () => {
+  expect(sum(2, 2)).toBe(4);
+});
+
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("two plus four is six", () => {
+  expect(2 + 4).toBe(6);
+});
+
+test("adding positive numbers is not zero", () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
